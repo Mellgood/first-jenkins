68ae730ee012183d1b5f27b8810939c89949fefe
Carlo Centofanti <carlocentos@gmail.com>
2022-03-09 13:20:47 +0100

    Initial commit

---
+++ src/main/java/com/example/firstjenkins/FirstJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firstjenkins/FirstJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
